2f9ec182f8d210fcec513e80e7ebcdb0e3c1f7bf
Umang Chauhan <umang4191@outlook.com>
2024-08-01 19:22:11 +0200

    include both routers in main app

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI, status
+from router import blog_router, user_router
 
 app = FastAPI()
+
+app.include_router(blog_router.blogrouter)
+app.include_router(user_router.userrouter)
